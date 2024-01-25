63c7466354bb0521f57c065b48db5365a663fa2c
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-01-25 14:28:49 +0100

    fix: adds field mappings

--- prosnet-prefect-pipelines/wikidata_index.py
+++ prosnet-prefect-pipelines/wikidata_index.py
@@ -1,5 +1,5 @@
 from string import Template
-from typing import Optional
+from typing import List, Optional
 from SPARQLWrapper import JSON, SPARQLWrapper
 from pydantic import BaseModel, Field, HttpUrl
 from prefect import task, flow
@@ -37,12 +37,14 @@ def create_sparql_queries(path_sparql_query, incremental_update, incremental_dat
 
 
 @task()
-def create_typesense_data_from_sparql_data(sparql_data):
+def create_typesense_data_from_sparql_data(sparql_data, field_mapping):
     """Create typesense data from SPARQL data."""
     res = []
     for item in sparql_data["results"]["bindings"]:
         res2 = {}
         for key, value in item.items():
+            if key in field_mapping:
+                key = field_mapping[key]
             if key == "id":
                 res2["id"] = value["value"].split("/")[-1]
             else:
@@ -60,8 +62,7 @@ class Params(BaseModel):
     path_sparql_query: str = Field(..., description="Relativ path to SPARQL query.")
     sparql_endpoint: HttpUrl = Field("https://query.wikidata.org/", description="SPARQL endpoint to use, defaults to wikidata.")
     limit: int = Field(500, description="Limit to use for the SPARQL queries")
-    typesense_definition: Optional[dict] = Field(
-        {
+    typesense_definition: dict = Field({
         "name": "prosnet-wikidata-person-index",
         "fields": [
             {"name": "id", "type": "string"},
@@ -79,10 +80,11 @@ class Params(BaseModel):
     typesense_collection_name: str = Field("prosnet-wikidata-person-index", description="Name of the typesense collection to use.")
     typesense_api_key: str = Field("typesense-api-key", description="Name of the Prefect secrets block that holds the API key to use for typesense.")
     typesense_host: str = Field("typesense.acdh-dev.oeaw.ac.at", description="Host to use for typesense.")
+    field_mapping: Optional[dict] = Field({"itemLabel": "label"}, description="List of tuples to map SPARQL fields to typesense fieldnames.")
 
 
 
-@flow(version="0.1.4")
+@flow(version="0.1.6")
 def create_typesense_index_from_sparql_query(params: Params):
     """Create a typesense index from a SPARQL data."""
     sparql_con = setup_sparql_connection(params.sparql_endpoint)
@@ -90,7 +92,7 @@ def create_typesense_index_from_sparql_query(params: Params):
     counts = retrieve_data_from_sparql_query(sparql_count_query, sparql_con, incremental_date=params.incremental_date, count_query=True)
     for offset in range(0, int(counts), params.limit):
         sparql_data = retrieve_data_from_sparql_query.submit(sparql_query, sparql_con, offset, params.limit, incremental_date=params.incremental_date)
-        typesense_data = create_typesense_data_from_sparql_data(sparql_data)
+        typesense_data = create_typesense_data_from_sparql_data(sparql_data, params.field_mapping)
         push_data_to_typesense_flow(PushParams(
             typesense_collection_name=params.typesense_collection_name,
             typesense_api_key=params.typesense_api_key,
